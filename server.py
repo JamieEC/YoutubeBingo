17b63d0b10ee7ef2393a8e3acfe9ae7960586796
Jamie <jamie.ec.96@gmail.com>
2026-03-18 17:42:46 +0000

    remove api key

--- server.py
+++ server.py
@@ -8,8 +8,7 @@ import sys
 app = Flask(__name__)
 # PLAYLIST_ID = "PLHw2hnQN_c5apYwWirtCoNgY83i3yu7un" # Initial
 PLAYLIST_ID = "PLYmHp8za33TxRaLpsed8olerJjOGRScKN" # Updated playlist with more videos
-# YOUTUBE_API_KEY = os.environ["YOUTUBE_API_KEY"]
-YOUTUBE_API_KEY = "AIzaSyCohI6jQU5XHukgBZJ9ft1iTDLp6Opy0f8"
+YOUTUBE_API_KEY = os.environ["YOUTUBE_API_KEY"]
 
 def iso8601_to_seconds(duration):
     return int(isodate.parse_duration(duration).total_seconds())
